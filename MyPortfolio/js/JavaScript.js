986299a7a0abf95d760a072abbcbf69ab4a7c503
Michael Brown <michaelfordbrown@hotmail.com>
2017-02-09 22:31:50 +0000

    Added jQuery format to default page to change colors and position floating image (added new JS file).

---
+++ MyPortfolio/js/JavaScript.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    //format footer
+    $('.navbar').css('background-color', 'black');
+
+    //format default
+    $('body').css('background-color', 'black');
+
+    //format headers
+    $('h1').css('color', 'lightblue');
+    $('h2').css('color', 'lightblue');
+    $('h2 *').css('color', 'lightblue');
+    $('h3').css('color', 'lightblue');
+    $('h3 *').css('color', 'lightblue');
+
+    //format highlight color
+    $('.highlight').css('color', 'white');
+
+    //format floating images
+    $('.floating').css('float', 'left');
+    $('.floating').css('margin-right', '10px');
+    $('.floating').css('margin-bottom', '10px');
+    $('.floating').css('margin-top', '5px');
+
+    //format jumbotron
+    $('.jumbotron').css('background-color', 'black');
+    $('.jumbotron *').css('text-align', 'center');
+    $('.jumbotron .ready').css('color', 'lightgreen');
+    $('.jumbotron').css('border-style', 'solid');
+    $('.jumbotron').css('border-width', '1px');
+    $('.jumbotron').css('border-color', 'grey');
+    
+    //format "project list" and "skills" asides
+    $('aside *').css('background-color', 'black');
+    $('aside p').css('color', 'grey');
+    $('aside .ready').css('color', 'lightgreen');
+    $('aside *').css('border-radius', '5px');
+
+    //format "profile" main article
+    $('article p').css('color', 'yellow');
+    $('article').css('text-align', 'justify');
+    $('article .ready').css('color', 'lightgreen');
+})
